b56ab042b49e1ad72242d79153edc74fd4f3bc3c
Mentalflow <312902918@qq.com>
2024-02-02 01:51:13 +0800

    CMakeLists and Workflow: optimize runtime generation.

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -27,7 +27,7 @@ function Main() {
 
     New-Item -ItemType Directory $archiveName
     # 拷贝exe
-    Copy-Item D:\a\RibbonUI\RibbonUI\build\example\* $archiveName\ -Force -Recurse | Out-Null
+    Copy-Item D:\a\RibbonUI\RibbonUI\build\app\release\* $archiveName\ -Force -Recurse | Out-Null
     # 拷贝依赖
     windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
     # 删除不必要的文件
